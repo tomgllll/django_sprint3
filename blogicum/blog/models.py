071d34b179585ada269cbd2bc465fd596d88fcf6
Тома Гладышева <tommmgll@gmail.com>
2024-10-26 13:48:28 +0300

    review 2

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
-MAX_LENGTH = 256
+MAX_TITLE_LENGTH = 256
 MAX_STR_LENGTH = 50
 
 
@@ -28,7 +28,7 @@ class BaseModel(models.Model):
 
 class Category(BaseModel):
     title = models.CharField(
-        max_length=MAX_LENGTH,
+        max_length=MAX_TITLE_LENGTH,
         verbose_name='Заголовок'
     )
     description = models.TextField(verbose_name='Описание')
@@ -49,7 +49,7 @@ class Category(BaseModel):
 
 class Location(BaseModel):
     name = models.CharField(
-        max_length=MAX_LENGTH,
+        max_length=MAX_TITLE_LENGTH,
         verbose_name='Название места'
     )
 
@@ -63,7 +63,7 @@ class Location(BaseModel):
 
 class Post(BaseModel):
     title = models.CharField(
-        max_length=MAX_LENGTH,
+        max_length=MAX_TITLE_LENGTH,
         verbose_name='Заголовок'
     )
     text = models.TextField(verbose_name='Текст')
@@ -75,29 +75,27 @@ class Post(BaseModel):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор публикации',
-        related_name='posts'
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name='Местоположение',
-        related_name='posts'
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Категория',
-        related_name='posts'
+        verbose_name='Категория'
     )
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        ordering = ['-pub_date']
+        ordering = ('-pub_date',)
+        default_related_name = 'posts'
 
     def __str__(self):
         return truncate_text(self.title)
